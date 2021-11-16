26ce889dbce8598fe0779867b4e422695c058e59
NaniNasser <Nasser.FAQIRI@student.intecbrussel.be>
2021-11-16 16:05:55 +0100

    added users/pets

--- src/main/java/com/example/petstore/service/User.java
+++ src/main/java/com/example/petstore/service/User.java
@@ -1,4 +1,23 @@
 package com.example.petstore.service;
 
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Accessors(chain = true)
 public class User {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String phone;
+    private long userStatus;
 }
