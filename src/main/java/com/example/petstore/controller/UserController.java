9c91464ee248af56c905c5c8e8fb7efd69498f0b
NaniNasser <Nasser.FAQIRI@student.intecbrussel.be>
2021-11-19 14:44:25 +0100

    Added user Nani

--- src/main/java/com/example/petstore/controller/UserController.java
+++ src/main/java/com/example/petstore/controller/UserController.java
@@ -18,12 +18,20 @@ public class UserController {
     UserDAO userDAO;
 
     @PostMapping
-    public ResponseEntity<ApiResponse> postUser(@RequestBody User user){
-        if (user.getId() != null && user.getId() > 0){
-            return ResponseEntity.badRequest()
+    public ResponseEntity<ApiResponse> postUser(@RequestBody User user) {
+        if (user.getId() != null && user.getId() > 0) {
+            return ResponseEntity.badRequest().body(new ApiResponse(404, "Not found", "User not found."));
+        } else {
+            userDAO.save(user);
+            return ResponseEntity.ok(new ApiResponse(200, "OK", "User saved to the database"));
         }
-
     }
-
 }
+
+
+
+
+
+
+
 
